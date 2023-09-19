73490a390fba4d14feb5b50b04c057ac6262b7b3
Adil-ha <hanoun.adil@gmail.com>
2023-09-19 17:25:16 +0200

    Ajout Tp Course

---
+++ javascript/TpCourse/script.js
@@ -0,0 +1,92 @@
+const btnAddProduct = document.querySelector(".btnAddProduct");
+const listProduct = [];
+let id = 0;
+const containListProduct = document.querySelector(".product-list-container");
+const reset = document.querySelector(".reset");
+
+window.addEventListener("load", () => {
+  const localstoragetab = JSON.parse(localStorage.getItem("product"));
+  if (localstoragetab) {
+    listProduct.push(...localstoragetab);
+    id = listProduct.length;
+    createProductInTable();
+  }
+});
+
+btnAddProduct.addEventListener("click", () => {
+  const productInput = document.querySelector(".productInput").value;
+  if (productInput != "") {
+    id++;
+    const product = {
+      id: id,
+      name: productInput,
+    };
+    listProduct.push(product);
+    const tabproduct = JSON.stringify(listProduct);
+    localStorage.setItem("product", tabproduct);
+    document.querySelector(".productInput").value = "";
+    createProductInTable();
+  } else {
+    alert("le champs est vide!");
+  }
+});
+
+reset.addEventListener("click", () => {
+  localStorage.clear();
+  location.reload();
+});
+
+function createProductInTable() {
+  containListProduct.innerHTML = "";
+  listProduct.forEach((currentProduct) => {
+    const ul = document.createElement("ul");
+    ul.setAttribute("data-id", currentProduct.id);
+    const li = document.createElement("li");
+    li.innerHTML = currentProduct.name;
+
+    const editButton = document.createElement("button");
+    editButton.innerHTML = "Éditer";
+    editButton.className = "btn btn-primary btn-sm mr-2";
+    editButton.addEventListener("click", () => {
+      alert("Fonction d'édition non implémentée.");
+    });
+
+    const deleteButton = document.createElement("button");
+    deleteButton.innerHTML = "Supprimer";
+    deleteButton.className = "btn btn-danger btn-sm";
+    deleteButton.addEventListener("click", () => {
+      const productId = currentProduct.id;
+      deleteProductById(productId);
+    });
+
+    ul.appendChild(li);
+    ul.appendChild(editButton);
+    ul.appendChild(deleteButton);
+    containListProduct.appendChild(ul);
+  });
+}
+
+function deleteProductById(id) {
+  const index = listProduct.findIndex((product) => product.id === id);
+  if (index !== -1) {
+    listProduct.splice(index, 1);
+    localStorage.setItem("product", JSON.stringify(listProduct));
+    const productElement = document.querySelector(`[data-id="${id}"]`);
+    if (productElement) {
+      productElement.remove();
+    }
+  }
+}
+
+function editProductById(id, newName) {
+  const index = listProduct.findIndex((product) => product.id === id);
+  if (index !== -1) {
+    listProduct[index].name = newName;
+    localStorage.setItem("product", JSON.stringify(listProduct));
+    const productElement = document.querySelector(`[data-id="${id}"]`);
+    if (productElement) {
+      const li = productElement.querySelector("li");
+      li.innerHTML = newName;
+    }
+  }
+}
