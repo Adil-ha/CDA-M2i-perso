558b17a30e97d04dc023efaf606da932abe11112
Adil-ha <hanoun.adil@gmail.com>
2023-12-18 14:13:49 +0100

    Ajout exo1 jdbc

--- java/JDBC/exercice/exercice_1/exercice_student.sql
+++ java/JDBC/exercice/exercice_1/exercice_student.sql
@@ -3,11 +3,11 @@ CREATE DATABASE college;
 USE college;
 
 CREATE TABLE T_student (
- id INT AUTO_INCREMENT PRIMARY KEY,
- last_name VARCHAR(200),
- first_name VARCHAR(200),
- number_class INT,
- date_diplome DATE
+ id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+ last_name VARCHAR(200) NOT NULL,
+ first_name VARCHAR(200) NOT NULL,
+ number_class INT NOT NULL,
+ date_diplome DATE NOT NULL
 );
 
 DROP TABLE student;
